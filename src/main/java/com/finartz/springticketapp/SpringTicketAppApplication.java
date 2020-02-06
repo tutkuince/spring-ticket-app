5292e875c962480aa48fcc93c0fab40d16b15a15
tutkuince <tutku.ince@outlook.com>
2020-02-06 22:34:44 +0300

    initial commit

---
+++ src/main/java/com/finartz/springticketapp/SpringTicketAppApplication.java
@@ -0,0 +1,13 @@
+package com.finartz.springticketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTicketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTicketAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finartz/springticketapp/SpringTicketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finartz.springticketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTicketAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
