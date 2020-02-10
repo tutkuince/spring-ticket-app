a0a0e5e5ea529c514e258e2d0e1eed0022c8344b
tutkuince <tutku.ince@outlook.com>
2020-02-10 17:44:48 +0300

    @EqualsAndHashCode(callSuper = false) @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)     @JsonFormat(pattern = "yyyy-MM-dd")

--- src/main/java/com/finartz/springticketapp/entity/Route.java
+++ src/main/java/com/finartz/springticketapp/entity/Route.java
@@ -1,7 +1,11 @@
 package com.finartz.springticketapp.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.finartz.springticketapp.entity.base.BaseEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -12,6 +16,8 @@ import java.util.Set;
 
 @Data
 @Entity
+@EqualsAndHashCode(callSuper = false)
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Route extends BaseEntity {
 
     @NotNull
@@ -21,6 +27,8 @@ public class Route extends BaseEntity {
     private String flightTo;
 
     @NotNull
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate departureDate;
 
     @OneToMany(mappedBy = "route", cascade = CascadeType.ALL)
