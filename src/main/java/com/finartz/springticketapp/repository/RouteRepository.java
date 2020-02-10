f3d9908495bfae6ebaf16d1614d325d462741bfc
tutkuince <tutku.ince@outlook.com>
2020-02-10 14:22:01 +0300

    getByFlightFromAndFlightToAndDepartureDate, getByFlightFromAndFlightTo and getByDepartureDate

--- src/main/java/com/finartz/springticketapp/repository/RouteRepository.java
+++ src/main/java/com/finartz/springticketapp/repository/RouteRepository.java
@@ -3,5 +3,11 @@ package com.finartz.springticketapp.repository;
 import com.finartz.springticketapp.entity.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface RouteRepository extends JpaRepository<Route, Long> {
+
+    Route getByFlightFromAndFlightToAndDepartureDate(String flightFrom, String flightTo, LocalDate departureDate);
+    Route getByFlightFromAndFlightTo(String flightFrom, String flightTo);
+    Route getByDepartureDate(LocalDate departureDate);
 }
