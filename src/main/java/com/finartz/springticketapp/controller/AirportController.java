08674d762b48c9a38938c426c0b959445557884d
tutkuince <tutku.ince@outlook.com>
2020-02-10 14:57:01 +0300

    getAirports

--- src/main/java/com/finartz/springticketapp/controller/AirportController.java
+++ src/main/java/com/finartz/springticketapp/controller/AirportController.java
@@ -3,12 +3,10 @@ package com.finartz.springticketapp.controller;
 import com.finartz.springticketapp.entity.Airport;
 import com.finartz.springticketapp.service.AirportService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/airports")
@@ -24,6 +22,12 @@ public class AirportController {
     public ResponseEntity<Airport> createAirport(@Valid @RequestBody Airport airport) {
         airportService.save(airport);
         return ResponseEntity.ok(airport);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Airport>> getAirports() {
+        List<Airport> airports = airportService.getAll();
+        return ResponseEntity.ok(airports);
     }
 
 }
