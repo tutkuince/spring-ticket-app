1d609bba692e8b871d96228c74928a8133819d46
tutkuince <tutku.ince@outlook.com>
2020-02-10 14:03:27 +0300

    getByName

--- src/main/java/com/finartz/springticketapp/repository/AirportRepository.java
+++ src/main/java/com/finartz/springticketapp/repository/AirportRepository.java
@@ -4,4 +4,6 @@ import com.finartz.springticketapp.entity.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AirportRepository extends JpaRepository<Airport, Long> {
+
+    Airport getByName(String name);
 }
