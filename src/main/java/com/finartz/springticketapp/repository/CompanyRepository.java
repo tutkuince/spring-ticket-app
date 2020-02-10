a21d4891ba6bb36a6ef8df6205801d8a21dc7c9a
tutkuince <tutku.ince@outlook.com>
2020-02-10 14:07:00 +0300

    getByName

--- src/main/java/com/finartz/springticketapp/repository/CompanyRepository.java
+++ src/main/java/com/finartz/springticketapp/repository/CompanyRepository.java
@@ -4,4 +4,6 @@ import com.finartz.springticketapp.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    Company getByName(String name);
 }
