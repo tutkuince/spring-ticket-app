ed0183a29e1460ff2e37068d7dedf96fc87bedb0
tutkuince <tutku.ince@outlook.com>
2020-02-10 15:09:02 +0300

    @EqualsAndHashCode(callSuper = false) @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})

--- src/main/java/com/finartz/springticketapp/entity/Airport.java
+++ src/main/java/com/finartz/springticketapp/entity/Airport.java
@@ -1,7 +1,9 @@
 package com.finartz.springticketapp.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.finartz.springticketapp.entity.base.BaseEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -11,6 +13,8 @@ import java.util.Set;
 
 @Entity
 @Data
+@EqualsAndHashCode(callSuper = false)
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Airport extends BaseEntity {
 
     @NotNull
