801bd668b93b966463982dc2b88f90a777f75d64
tutkuince <tutku.ince@outlook.com>
2020-02-10 16:37:47 +0300

    getByCompany

--- src/main/java/com/finartz/springticketapp/repository/FlightRepository.java
+++ src/main/java/com/finartz/springticketapp/repository/FlightRepository.java
@@ -1,7 +1,12 @@
 package com.finartz.springticketapp.repository;
 
+import com.finartz.springticketapp.entity.Company;
 import com.finartz.springticketapp.entity.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+    List<Flight> getByCompany(Company company);
 }
