1025572bbc14249e7cf8e55dbf7d61bf24d2567a
tutkuince <tutku.ince@outlook.com>
2020-02-10 14:33:24 +0300

    getByTicketNumber

--- src/main/java/com/finartz/springticketapp/repository/TicketRepository.java
+++ src/main/java/com/finartz/springticketapp/repository/TicketRepository.java
@@ -4,4 +4,6 @@ import com.finartz.springticketapp.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    Ticket getByTicketNumber(String ticketNumber);
 }
