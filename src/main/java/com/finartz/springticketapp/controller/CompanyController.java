98ce40794c25a778981ef1a384ac284210da0f3e
tutkuince <tutku.ince@outlook.com>
2020-02-10 16:26:48 +0300

    CompanyController

--- src/main/java/com/finartz/springticketapp/controller/CompanyController.java
+++ src/main/java/com/finartz/springticketapp/controller/CompanyController.java
@@ -3,12 +3,10 @@ package com.finartz.springticketapp.controller;
 import com.finartz.springticketapp.entity.Company;
 import com.finartz.springticketapp.service.CompanyService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/companies")
@@ -23,6 +21,24 @@ public class CompanyController {
     @PostMapping
     public ResponseEntity<Company> createCompany(@Valid @RequestBody Company company) {
         companyService.save(company);
+        return ResponseEntity.ok(company);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Company>> getCompanies() {
+        List<Company> companies = companyService.getAll();
+        return ResponseEntity.ok(companies);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable Long id) {
+        Company company = companyService.getById(id);
+        return ResponseEntity.ok(company);
+    }
+
+    @GetMapping("/company")
+    public ResponseEntity<Company> getCompanyByName(@RequestParam(value = "name") String name) {
+        Company company = companyService.getByName(name);
         return ResponseEntity.ok(company);
     }
 }
